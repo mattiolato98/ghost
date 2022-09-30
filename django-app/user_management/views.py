fae388c9f40a6f6a73926f39818acd4cf4c06476
gabrymattioli <gabrymattioli@gmail.com>
2022-09-30 18:00:02 +0200

    user_management, views: added SettingsTemplateView

--- django-app/user_management/views.py
+++ django-app/user_management/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class SettingsTemplateView(TemplateView):
+    template_name = 'user_management/settings.html'
