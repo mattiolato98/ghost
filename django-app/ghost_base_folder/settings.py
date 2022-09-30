c580d700e8929a481e768fe58cc3ab4a4ec2584c
gabrymattioli <gabrymattioli@gmail.com>
2022-09-30 12:31:41 +0200

    Set bootstrap5 as crispy template pack

--- django-app/ghost_base_folder/settings.py
+++ django-app/ghost_base_folder/settings.py
@@ -139,3 +139,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "user_management.PlatformUser"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
