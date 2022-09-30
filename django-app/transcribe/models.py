199e68afcfb23aca48969b7d6211c1e00c51f96a
gabrymattioli <gabrymattioli@gmail.com>
2022-09-30 15:26:51 +0200

    Created model Transcription. It contains single audio and its transcription.

--- django-app/transcribe/models.py
+++ django-app/transcribe/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Transcription(models.Model):
+    """
+    Model that describe an audio file.
+    """
+    name = models.CharField(max_length=200)
+    audio = models.FileField(upload_to='uploads/%Y/%m/%d')
+    text = models.TextField()
+
+    create_datetime = models.DateTimeField(auto_now_add=True)
+    last_edit = models.DateTimeField(auto_now=True)
+
+    transcribed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-last_edit', '-create_datetime']
