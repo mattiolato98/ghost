8f6a53d1414475c534a7e8d3f7568222555ce178
gabrymattioli <gabrymattioli@gmail.com>
2022-10-03 17:32:34 +0200

    Transcribe, views.py: added object retrieval if pk is present

--- django-app/transcribe/views.py
+++ django-app/transcribe/views.py
@@ -16,3 +16,17 @@ class TranscriptionListView(ListView):
     model = Transcription
     template_name = 'transcribe/transcription_list.html'
     context_object_name = 'audios'
+    object = None
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(TranscriptionListView, self).get(request, *args, **kwargs)
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', None)
+        return Transcription.objects.get(pk=pk) if pk is not None else None
+
+    def get_context_data(self, **kwargs):
+        context = super(TranscriptionListView, self).get_context_data(**kwargs)
+        context['audio_object'] = self.object
+        return context
