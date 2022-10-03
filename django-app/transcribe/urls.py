3c4b64951259fe2f9710e352aae27fac2cbe78c4
gabrymattioli <gabrymattioli@gmail.com>
2022-10-03 17:31:54 +0200

    Transcribe, urls.py: added TranscriptionListView URL with pk argument

--- django-app/transcribe/urls.py
+++ django-app/transcribe/urls.py
@@ -6,4 +6,5 @@ app_name = 'transcribe'
 urlpatterns = [
     path('form', views.TranscriptionCreateView.as_view(), name='transcription-form'),
     path('list', views.TranscriptionListView.as_view(), name='transcription-list'),
+    path('list/<int:pk>', views.TranscriptionListView.as_view(), name='transcription-list'),
 ]
