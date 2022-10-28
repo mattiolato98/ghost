987487e42e44903222b502c76b217978f64f22c5
Filippo Fontana <filippofontana1998@gmail.com>
2022-10-28 17:05:26 +0200

    scripts, djagno-run: fixed django base folder name in gunicorn command

--- scripts/django_run.sh
+++ scripts/django_run.sh
@@ -7,7 +7,7 @@ if [[ -z $DEBUG ]]; then
     python manage.py collectstatic --noinput
     python manage.py migrate
 
-    gunicorn load_balancer_ws.wsgi:application --bind 0.0.0.0:$GHOST_WEB_APP_PORT
+    gunicorn ghost_base_folder.wsgi:application --bind 0.0.0.0:$GHOST_WEB_APP_PORT
 else
     python manage.py wait_for_db
     python manage.py migrate
