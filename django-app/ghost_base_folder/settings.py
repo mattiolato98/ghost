9591081a99d7b54fb251dcce948aff413a506eda
Filippo Fontana <filippofontana1998@gmail.com>
2022-10-28 17:07:41 +0200

    django-app, settings: retrieved STATIC_ROOT from env variable

--- django-app/ghost_base_folder/settings.py
+++ django-app/ghost_base_folder/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.environ.get('GHOST_STATIC_FILE_FOLDER')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
