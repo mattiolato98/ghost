298a6eb7d56ad641ef65904425356a4dac78ac60
Filippo Fontana <filippofontana1998@gmail.com>
2022-10-31 13:58:31 +0100

    transcribe, views: removed unused imports

--- django-app/transcribe/views.py
+++ django-app/transcribe/views.py
@@ -2,10 +2,9 @@ import os.path
 
 import celery
 
-from django.conf import settings as django_settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.mail import mail_admins, EmailMultiAlternatives
+from django.core.mail import mail_admins
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
