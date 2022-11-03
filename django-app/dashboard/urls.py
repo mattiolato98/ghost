a17534ea8575191df29185ed68f9db7337136e1c
gabrymattioli <gabrymattioli@gmail.com>
2022-11-03 17:50:16 +0100

    dashboard, urls: reformat

--- django-app/dashboard/urls.py
+++ django-app/dashboard/urls.py
@@ -5,6 +5,6 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('home', views.DashboardTemplateView.as_view(), name='home'),
-    path('token_list', views.SignInTokenListView.as_view(), name='token-list'),
-    path('generate_token', views.generate_token, name='generate-token'),
+    path('token-list', views.SignInTokenListView.as_view(), name='token-list'),
+    path('generate-token', views.generate_token, name='generate-token'),
 ]
