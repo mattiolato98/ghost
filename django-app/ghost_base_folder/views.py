350f9e295e733137257b678876290357280d8412
Filippo Fontana <filippofontana1998@gmail.com>
2022-12-02 10:58:13 +0100

    django-app, views: added not_authenticated_only decorator to homepage view

--- django-app/ghost_base_folder/views.py
+++ django-app/ghost_base_folder/views.py
@@ -1,5 +1,9 @@
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
+from user_management.decorators import not_authenticated_only
 
+
+@method_decorator(not_authenticated_only, name='dispatch')
 class HomeTemplateView(TemplateView):
     template_name = 'home.html'
